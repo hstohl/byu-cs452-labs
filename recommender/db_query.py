3347250a9adb778fa87b623959a2b0be56ac781a
Hudson Stohl <hrstohl02@gmail.com>
2025-05-15 22:15:18 -0600

    first three queries

--- recommender/db_query.py
+++ recommender/db_query.py
@@ -7,3 +7,34 @@ load_dotenv()
 
 CONNECTION = os.getenv("DATABASE_URL") # paste connection string here or read from .env file
 
+query_1 = """
+    SELECT p.title, ps.id, ps.content, ps.start_time, ps.end_time, ps.embedding <-> target.embedding AS embedding_distance
+    FROM podcast_segment ps
+    JOIN podcast p ON ps.podcast_id = p.id,
+        (SELECT embedding FROM podcast_segment WHERE id = '267:476') AS target
+    WHERE ps.id != '267:476'
+    ORDER BY embedding_distance
+    LIMIT 5;
+"""
+
+query_2 = """
+    SELECT p.title, ps.id, ps.content, ps.start_time, ps.end_time, ps.embedding <-> target.embedding AS embedding_distance
+    FROM podcast_segment ps
+    JOIN podcast p ON ps.podcast_id = p.id,
+        (SELECT embedding FROM podcast_segment WHERE id = '267:476') AS target
+    WHERE ps.id != '267:476'
+    ORDER BY embedding_distance DESC
+    LIMIT 5;
+"""
+
+query_3 = """
+    SELECT p.title, ps.id, ps.content, ps.start_time, ps.end_time, ps.embedding <-> target.embedding AS embedding_distance
+    FROM podcast_segment ps
+    JOIN podcast p ON ps.podcast_id = p.id,
+        (SELECT embedding FROM podcast_segment WHERE id = '48:511') AS target
+    WHERE ps.id != '48:511'
+    ORDER BY embedding_distance
+    LIMIT 5;
+"""
+
+
